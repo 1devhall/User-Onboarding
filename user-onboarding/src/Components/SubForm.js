4a11938bcaf1641b2cd38302bd747fd38a4cfe9d
1devhall <devoughnh@gmail.com>
2020-01-31 10:33:16 -0500

    done

--- user-onboarding/src/Components/SubForm.js
+++ user-onboarding/src/Components/SubForm.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
-import {withFormik, Form, Field, yupToFormErrors} from "formik";
+import React, {useState, useEffect} from 'react';
+import {withFormik, Form, Field, yupToFormErrors, prepareDataForValidation} from "formik";
 import * as Yup from 'yup';
 import axios from 'axios'
 
 
 const SubForm = ({status, values, errors, touched}) => {
 
-    const [info, setInfo] = useState([]);
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        
+        console.log('status has changed', status);
+        status && setUsers(info => [...users, status]);
     }, [status]);
 
    
     return(
-
+        <div>
         <Form>
             <div>
-                <label htmlFor='name'>Name:</label>
+                <label htmlFor='name'>Username:</label>
                 <Field  
                  type='text'
                  name= 'name' 
@@ -26,7 +27,7 @@ const SubForm = ({status, values, errors, touched}) => {
                {touched.name && errors.name && <p>{errors.name}</p>}
             </div>
             <div>
-                <label htmlFor='email'>email:</label>
+                <label htmlFor='email'>Email:</label>
                 <Field  
                 type='email' 
                 name= 'email' 
@@ -35,9 +36,9 @@ const SubForm = ({status, values, errors, touched}) => {
                 {touched.email && errors.email && <p>{errors.email}</p>}
             </div>
             <div>
-                <label htmlFor='password'>password:</label>
+                <label htmlFor='password'>Password:</label>
                 <Field
-                 type='text'
+                 type='password'
                  name= 'password'
                  placeholder= 'Password'
                />
@@ -54,9 +55,19 @@ const SubForm = ({status, values, errors, touched}) => {
             </div>
             <button type='submit'>Submit</button>
         </Form>
-    )
+
+        {users.map(subInfo => {
+          return(
+              <div>
+            <p>Name:{subInfo.name}</p>
+            <p>Email:{subInfo.email}</p>
+            </div>
+          );
+        })}
+        </div>
+    );
     
-}
+};
 const FormikForm = withFormik({
     mapPropsToValues({name, email, password, TOS}){
         return {
@@ -71,10 +82,10 @@ const FormikForm = withFormik({
         name: Yup.string().required('Must enter name'),
         email: Yup.string().required('Must enter email'),
         password: Yup.string().required('Must enter password'),
-        TOS: Yup.string().required('Must agree to the terms')
+        TOS: Yup.boolean().required('Must agree to the terms')
     }),
     handleSubmit(values, {setStatus}){
-        axios.post('https://reqres.in/api/users')
+        axios.post('https://reqres.in/api/users', values)
         .then(res => {
             console.log('success', res);
             setStatus(res.data)
